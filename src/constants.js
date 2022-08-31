9e06a973860aa38b809d3f439d4f7f55ff6d798c
sanedolt <sanedolt@gmail.com>
2022-08-30 23:04:34 -0500

    first commit

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "MBECC9-ZCX65Y-RLZLGM-4WEM";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://3.18.112.37";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
